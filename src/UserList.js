ac25e12f30db1793c5be029fc5205139f2d0b9bd
aichaabdelali <aicha.abdelali6@gmail.com>
2020-10-02 04:20:23 +0200

    Checkpoint API

---
+++ src/UserList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://jsonplaceholder.typicode.com/users");
+      console.log(result);
+      setData(result.data);
+      console.log(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <a className="username" href={user.username}>
+              {user.username}
+            </a>
+            <a href={user.name}>{user.name}</a>
+            <a href={user.email}>{user.email}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
